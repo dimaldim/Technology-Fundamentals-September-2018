fc62c10d7258dd99dd9cd2f51758b307d7e64460
dimaldim <antraks92@gmail.com>
2018-11-21 17:56:22 +0200

    Upload

---
+++ JS Core/Control Flow Logic/10. Chess Board.js
@@ -0,0 +1,14 @@
+function chessBoard(n) {
+    let html = '<div class="chessboard">\n';
+    for (let i = 0; i < n; i++) {
+        html += `  <div>\n`;
+        let color = (i % 2 === 0) ? 'black' : 'white';
+        for (let j = 0; j < n; j++) {
+            html += `    <span class="${color}"></span>\n`;
+            color = (color === 'white') ? 'black' : 'white';
+        }
+        html += '</div>\n';
+    }
+    html += '</div>';
+    console.log(html);
+}
