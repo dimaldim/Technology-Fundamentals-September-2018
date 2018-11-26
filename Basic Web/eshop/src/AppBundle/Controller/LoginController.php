83b609002c401b2b0df065f3610ed27c971f1475
dimaldim <antraks92@gmail.com>
2018-11-26 09:37:04 +0200

    Upload

---
+++ Basic Web/eshop/src/AppBundle/Controller/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends Controller
+{
+    /**
+     * @Route("/login", name="login")
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'default/login.html.twig',
+            [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]
+        );
+    }
+
+    /**
+     * @Route("/logout",name="logout")
+     * @throws \RuntimeException
+     */
+    public function logout()
+    {
+      throw new \RuntimeException('This should never be called directly');
+    }
+}
